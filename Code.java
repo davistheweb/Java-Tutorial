14bdb69609fa02be2171d4e6c1b96971a39304b4
davistheweb <smartkelvin775@gmail.com>
2024-06-13 11:33:25 +0100

    new changes

--- Code.java
+++ Code.java
@@ -1,5 +1,5 @@
 public class Code{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello, I am Josiah and I am learning Java 🙂↕");
     }
 }
