a19851d96b6b95593ddef165885d4a4f106a7020
davistheweb <smartkelvin775@gmail.com>
2024-06-13 11:57:33 +0100

    new chnages

---
+++ condit.java
@@ -0,0 +1,13 @@
+public class condit{
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+
+        if(num1 > num2) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
