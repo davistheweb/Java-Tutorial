d38d5b1446c490070959f00ebe6f4913b1db2e55
davistheweb <smartkelvin775@gmail.com>
2024-06-16 05:20:22 +0100

    made changes on readme.md

--- src/Var.java
+++ src/Var.java
@@ -2,7 +2,19 @@ package src;
 
 public class Var {
 public static void main(String[] args){
-    int age = 18;
+    int age = 18; // Defined an integer variable "age" with a given value "18"
     System.out.println(age);
+    age = 20; // changed the value of age after the first printing
+    System.out.println(age);
+    // Declaring multiple integer variable in one line goes as>>
+    int FirstMultiInt = 10, SecondMultiInt = 20, RdInt = 50;
+    System.out.println(FirstMultiInt);
+    System.out.println(SecondMultiInt);
+    System.out.println(RdInt);
+    //Coping of variable
+    //Example
+    int originVar = 40;
+    int newCopy = originVar;
+    System.out.println(newCopy);
 }
 }
