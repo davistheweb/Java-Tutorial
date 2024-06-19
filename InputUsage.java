6b6ba176feef57560b1a461dfa6ba7fb882ed20e
davistheweb <smartkelvin775@gmail.com>
2024-06-19 02:18:18 +0100

    :

--- InputUsage.java
+++ InputUsage.java
@@ -7,6 +7,16 @@ class Inputsage {
         System.out.print("Enter your age: ");
         //System.out.println("Hello " +name+" How are you doing");
         int age = input.nextInt();
-        System.out.println("Hello " +name +", You are "+age +" Years old");
+        String checkAge = null;
+        if(age > 30) {
+            checkAge = "Oldest";
+        }
+        else if(age > 20) {
+            checkAge = "Older";
+        }
+        else if(age < 20) {
+            checkAge = "Younger";
+        }
+        System.out.println("Hello " +name +", You are "+age +" Years "+checkAge);
     }
 }
